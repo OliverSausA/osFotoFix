d044dcd1261f5c4219d46f5925ce44e24890addf
Oliver <oliver@seidel-it.com>
2020-11-09 18:52:54 +0100

    Model FotoInfo erstellt

---
+++ osFotoFix/Models/FotoInfo.cs
@@ -0,0 +1,40 @@
+using System;
+using System.IO;
+
+namespace osFotoFix.Models
+{
+  public class FotoInfo {
+
+    public FotoInfo( FileInfo file, DateTime created, bool isExifValid ) {
+      File = file;
+      Created = created;
+      IsExifValid = isExifValid;
+    }
+
+    public FileInfo File {get;set;}
+
+    public DateTime Created {get;set;}
+
+    public bool IsExifValid {get;set;}
+    public string FileLocationOld {get;set;}
+    public string FileNameOld {get;set;}
+    public string FileLocationNew {get;set;}
+    public string FileNameNew {get;set;}
+
+
+    public override string ToString()
+    {
+      return string.Format( "{0} | {1} | {2} | {3} | {4} | {5} | {6} | {7}",
+        IsExifValid, 
+        Created.ToString("yyyy_MM"), 
+        Created.ToString("yyyyMMdd_hhmmss"), 
+        File.Name,
+        FileLocationOld,
+        FileLocationNew,
+        FileNameOld,
+        FileNameNew
+        );
+    }
+  }
+
+}
