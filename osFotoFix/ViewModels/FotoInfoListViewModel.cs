118afadf1344e0d8053dea778e5e1ac5a808a1df
Oliver <oliver@seidel-it.com>
2020-11-13 15:14:37 +0100

    FotoInfoList mit DataGrid darstellen

--- osFotoFix/ViewModels/FotoInfoListViewModel.cs
+++ osFotoFix/ViewModels/FotoInfoListViewModel.cs
@@ -11,12 +11,28 @@ namespace osFotoFix.ViewModels
     public FotoInfoListViewModel()
     {
       FotoInfoList = new ObservableCollection<FotoInfo>();
-      FotoInfoList.Add(
-          new FotoInfo( new FileInfo( "TEST.jpg"), DateTime.Now, true ) );
-      FotoInfoList.Add(
-          new FotoInfo( new FileInfo( "TEST2.jpg"), DateTime.Now, false ) );
+
+      var fotoInfo = new FotoInfo( new FileInfo( "TEST.jpg"), DateTime.Now, true );
+      fotoInfo.FileLocationOld = "FileLocaltionOld";
+      fotoInfo.FileNameOld = "FileNameOle";
+      fotoInfo.FileLocationNew = "FileLocaltionNew";
+      fotoInfo.FileNameNew = "FileNameNew";
+      FotoInfoList.Add( fotoInfo );
+
+      fotoInfo = new FotoInfo( new FileInfo( "TEST.jpg"), DateTime.Now, true );
+      fotoInfo.FileLocationOld = "FileLocaltionOld 2";
+      fotoInfo.FileNameOld = "FileNameOle 2";
+      fotoInfo.FileLocationNew = "FileLocaltionNew 2";
+      fotoInfo.FileNameNew = "FileNameNew 2";
+      FotoInfoList.Add( fotoInfo );
     }
 
-    public ObservableCollection<FotoInfo> FotoInfoList {get; set;}
+    public ObservableCollection<FotoInfo> FotoInfoList { get; set; }
+
+    private FotoInfo fotoSelected;
+    public FotoInfo FotoSelected { 
+      get { return fotoSelected; } 
+      set { fotoSelected = value; } 
+    }
   }
 }
